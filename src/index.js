c3c9a0739c6dfcf7853c9b92b59955e2b680c287
Mohammad <mhmmdrzaei@gmail.com>
2022-11-21 15:56:29 -0500

    adding cart icon useContext for a toggle on shopping cart

--- src/index.js
+++ src/index.js
@@ -6,17 +6,27 @@ import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import {UserProvider} from './contexts/user.context';
+import { ProductsProvider } from './contexts/products.context.jsx'
+import { CartProvider } from './contexts/cart.context.jsx'
+
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-  <BrowserRouter>
-  <UserProvider>
-    <App />
-  </UserProvider>
-  </BrowserRouter>
-    
+    <BrowserRouter>
+      <UserProvider>
+      
+        <ProductsProvider>
+        
+        <CartProvider>
+           <App />
+        </CartProvider>
+         
+        </ProductsProvider>
+
+      </UserProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,13 +1,19 @@
 import { Fragment, useContext } from 'react';
 import { Outlet, Link } from 'react-router-dom';
-import { ReactComponent as CrwnLogo} from '../../assets/crown.svg'
-import {UserContext} from '../../contexts/user.context'
+import { ReactComponent as CrwnLogo} from '../../assets/crown.svg';
+
+import CartIcon from '../../components/cart-icon/cart-icon.component';
+import CartDropDown from '../../components/cart-dropdown/cart-dropdown.component';
+
+import {UserContext} from '../../contexts/user.context';
+import {CartContext} from '../../contexts/cart.context';
 import { signOutUser } from '../../utils/firebase/firebase.utils'
 import './navigation.styles.scss';
 
 
 const Navigation = () => {
   const { currentUser } = useContext(UserContext);
+  const { isCartOpen } = useContext(CartContext);
 
   return (
 
@@ -30,10 +36,12 @@ const Navigation = () => {
               
               ): ( <Link className="nav-link" to="/auth">Sign-In</Link>)
           }
-          
+          <CartIcon />
+
 
 
         </div>
+          {isCartOpen && <CartDropDown />}
       </div>
       <Outlet /> 
     </Fragment>
